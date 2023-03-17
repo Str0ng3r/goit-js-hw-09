0e1292d484d975a867cb5c5d281bc95fc658ccc2
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-03-17 12:48:55 +0100

    UP

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+'use strict'
+const buttonStart = document.querySelector('[data-start]')
+const buttonStop = document.querySelector('[data-stop]')
+const bodyDoc = document.querySelector('body')
+console.log(buttonStart)
+console.log(buttonStop)
+console.log(bodyDoc)
+let interId = null;
 
+buttonStop.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+
+buttonStart.addEventListener('click', () => {
+    buttonStart.disabled = true;
+    interId = setInterval(() => {
+bodyDoc.style.backgroundColor = getRandomHexColor();
+   },1000)
+   buttonStop.disabled = false;
+})
+
+  buttonStop.addEventListener('click',() => {
+clearInterval(interId)
+buttonStop.disabled = true;
+buttonStart.disabled = false;
+  })
