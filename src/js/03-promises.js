1db4f6c77c01fc6bd8462016d48750c8e4a1ca01
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-03-19 19:26:13 +0100

    UPDATE

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,34 @@
 'use strict'
 
-const delayPromise = document.getElementsByName('#delay');
-const stepDelayPromise = document.getElementsByName('#step');
-const amountPromise = document.getElementsByName('#amount');
+const delayPromise = document.getElementsByName('delay');
+const stepPromise = document.getElementsByName('step');
+const amountPromise = document.getElementsByName('amount');
 const form = document.querySelector('.form')
- console.log(delayPromise)
- console.log(stepDelayPromise)
- console.log(amountPromise)
-
-const valueStep = stepDelayPromise.value;
-const valueDelay = delayPromise.value;
-const valueAmount = amountPromise.value;
 
 
-stepDelayPromise.addEventListener('click',()=> {
-  console.log(valueStep)
-})
 
 form.addEventListener('submit',(event) => {
   event.preventDefault();
-  console.log('submit')
+
+  const amount = Number(amountPromise[0].value);
+  const step = Number(stepPromise[0].value);
+  const delay = Number(delayPromise[0].value);
+
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+    const delayPromise = delay + (i-1) * step;
+    createPromise(position, delayPromise)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
 });
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-
-
 
   const promisFunc = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -35,7 +38,6 @@ const acceptRes = {
   position:position,
   delay:delay
 }
-      
     resolve(acceptRes)
       } else {
         // Reject
